7776a5535973ea20d8fd8ed1c8361c55f6710f4c
hsh <tjdgus3537@naver.com>
2019-10-25 02:23:59 +0900

    edit debug message

--- app/services/missionService.py
+++ app/services/missionService.py
@@ -114,9 +114,7 @@ class MissionServiceServicer(MissionServiceServicer, metaclass=ServicerMeta):
                                 str(mission_type) + "  " + str(DATA_TYPE[data_type]) + "  " + str(unit_package) + "  "+\
                                 str(price_of_package) + "  " + str(datetime.datetime(year=d_year,month=d_month,day=d_day,hour=d_hour,minute=d_min,second=d_sec)) \
                                      + "  " + str(order_package_quantity) + "  " + summary + "  " + contact_clause + "  " \
-                                     + specification + "  " + str(MISSION_STATE[mission_state]) + "  " + str(datetime.datetime.now()) + "  " \
-                + str(type(datetime.datetime(year=d_year,month=d_month,day=d_day,hour=d_hour,minute=d_min,second=d_sec))) + " " \
-                + str(type(datetime.datetime.now()))
+                                     + specification + "  " + str(MISSION_STATE[mission_state]) + "  " + str(datetime.datetime.now()) + "  "
                 result_message = error_message
                 register_mission_result = RegisterMissionResult.FAIL_REGISTER_MISSION_RESULT
 
