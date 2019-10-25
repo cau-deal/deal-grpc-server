1657493ad2917773ca2579661aec799f97f617e6
hsh <tjdgus3537@naver.com>
2019-10-26 02:49:59 +0900

    Refactoring structure

--- app/services/pointService.py
+++ app/services/pointService.py
@@ -3,6 +3,7 @@ from sea.servicer import ServicerMeta
 
 from app.decorators import verified, unverified
 from app.extensions import pwdb
+from app.extensions_db import sPointServicer
 
 from app.models import DepositPoint
 from app.models import WithdrawPoint
@@ -18,41 +19,6 @@ from protos.CommonResult_pb2 import *
 from peewee import fn
 
 import datetime
-
-class SPointerServicer():
-    def __init__(self):
-        pass
-
-    def sLookUpBalance(self, email):
-        balance = 0
-
-        db = pwdb.database
-
-        total_deposit_query = (DepositPoint.select(fn.Sum(DepositPoint.val).alias('total'))
-                                 .where(DepositPoint.user_email == email))
-        total_withdraw_query = (WithdrawPoint.select(fn.Sum(WithdrawPoint.val).alias('total'))
-                                 .where(WithdrawPoint.user_email == email))
-        total_receive_query = (TransferPoint.select(fn.Sum(TransferPoint.val).alias('total'))
-                                 .where(TransferPoint.receiver_email == email))
-        total_send_query = (TransferPoint.select(fn.Sum(TransferPoint.val).alias('total'))
-                                 .where(TransferPoint.sender_email == email))
-
-        for row in total_deposit_query:
-            if row.total is not None:
-                balance += row.total
-        for row in total_withdraw_query:
-            if row.total is not None:
-                balance -= row.total
-        for row in total_receive_query:
-            if row.total is not None:
-                balance += row.total
-        for row in total_send_query:
-            if row.total is not None:
-                balance -= row.total
-
-        return int(balance)
-
-sPointServicer = SPointerServicer()
 
 class PointServiceServicer(PointServiceServicer, metaclass=ServicerMeta):
     @verified
