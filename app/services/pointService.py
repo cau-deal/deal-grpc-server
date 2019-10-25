82c6ea6cc9758c45a58aafc88e5daeb63a69b865
hsh <tjdgus3537@naver.com>
2019-10-26 02:00:43 +0900

    seperate method

--- app/services/pointService.py
+++ app/services/pointService.py
@@ -21,7 +21,7 @@ import datetime
 
 
 class PointServiceServicer(PointServiceServicer, metaclass=ServicerMeta):
-
+    """
     @verified
     def LookUpBalance(self, request, context):
         result_code = ResultCode.UNKNOWN_RESULT_CODE
@@ -70,6 +70,64 @@ class PointServiceServicer(PointServiceServicer, metaclass=ServicerMeta):
                 message=result_message,
             ),
             balance=int(balance),
+        )
+        """
+    def sLookUpBalance(email):
+        balance = 0
+
+        db = pwdb.database
+
+        with db.atomic() as transaction:
+            total_deposit_query = (DepositPoint.select(fn.Sum(DepositPoint.val).alias('total'))
+                                 .where(DepositPoint.user_email == email))
+            total_withdraw_query = (WithdrawPoint.select(fn.Sum(WithdrawPoint.val).alias('total'))
+                                 .where(WithdrawPoint.user_email == email))
+            total_receive_query = (TransferPoint.select(fn.Sum(TransferPoint.val).alias('total'))
+                                 .where(TransferPoint.receiver_email == email))
+            total_send_query = (TransferPoint.select(fn.Sum(TransferPoint.val).alias('total'))
+                                 .where(TransferPoint.sender_email == email))
+
+            for row in total_deposit_query:
+                if row.total != None:
+                    balance += row.total
+            for row in total_withdraw_query:
+                if row.total != None:
+                    balance -= row.total
+            for row in total_receive_query:
+                if row.total != None:
+                    balance += row.total
+            for row in total_send_query:
+                if row.total != None:
+                    balance -= row.total
+
+        return int(balance)
+
+    @verified
+    def LookUpBalance(self, request, context):
+        result_code = ResultCode.UNKNOWN_RESULT_CODE
+        result_message = "Unknown Look up balance Result"
+
+        db = pwdb.database
+
+        with db.atomic() as transaction:
+            try:
+                balance = self.sLookUpBalance(context.login_email)
+
+                result_code = ResultCode.SUCCESS
+                result_message = "Look up balance success"
+
+            except Exception as e:
+                transaction.rollback()
+                result_code = ResultCode.ERROR
+                result_message = str(e)
+                print("EXCEPTION: " + str(e))
+
+        return LookUpBalanceResponse(
+            result=CommonResult(
+                result_code=result_code,
+                message=result_message,
+            ),
+            balance=balance,
         )
 
     @verified
