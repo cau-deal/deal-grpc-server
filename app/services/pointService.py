c4e0e20364527f1c7b89bcbe55ca16bd1b176780
hsh <tjdgus3537@naver.com>
2019-10-25 23:37:31 +0900

    look up balance bug test

--- app/services/pointService.py
+++ app/services/pointService.py
@@ -31,16 +31,14 @@ class PointServiceServicer(PointServiceServicer, metaclass=ServicerMeta):
 
         with db.atomic() as transaction:
             try:
-                """
                 total_deposit = (DepositPoint.select(fn.Sum(DepositPoint.val))
-                                 .where(DepositPoint.user_email==context.login_email)).scalar()
+                                 .where(DepositPoint.user_email==context.login_email))
                 total_withdraw = (WithdrawPoint.select(fn.Sum(WithdrawPoint.val))
-                                 .where(WithdrawPoint.user_email==context.login_email)).scalar()
+                                 .where(WithdrawPoint.user_email==context.login_email))
                 total_receive = (TransferPoint.select(fn.Sum(TransferPoint.val))
-                                 .where(TransferPoint.receiver_email==context.login_email)).scalar()
+                                 .where(TransferPoint.receiver_email==context.login_email))
                 total_send = (TransferPoint.select(fn.Sum(TransferPoint.val))
-                                 .where(TransferPoint.sender_email==context.login_email)).scalar()
-                """
+                                 .where(TransferPoint.sender_email==context.login_email))
 
                 result_code = ResultCode.SUCCESS
                 result_message = "Look up balance success"
@@ -48,7 +46,7 @@ class PointServiceServicer(PointServiceServicer, metaclass=ServicerMeta):
             except Exception as e:
                 transaction.rollback()
                 result_code = ResultCode.ERROR
-                result_message = str(e)
+                result_message = str(e) + "  " + str(total_deposit.sum)
                 print("EXCEPTION: " + str(e))
 
         return LookUpBalanceResponse(
