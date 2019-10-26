b9383aeb95409eb77c0aa8583d1175e41cc49e8d
hsh <tjdgus3537@naver.com>
2019-10-26 19:19:24 +0900

    CountNoReadPush

--- app/services/NotificationService.py
+++ app/services/NotificationService.py
@@ -1,3 +1,4 @@
+from peewee import fn
 from sea.servicer import ServicerMeta
 
 from app.decorators import verified
@@ -44,7 +45,7 @@ class NotificationServiceServicer(NotificationServiceServicer, metaclass=Service
             except Exception as e:
                 transaction.rollback()
                 result_code = ResultCode.ERROR
-                result_message = str(e) + str(type(context.login_email))
+                result_message = str(e)
                 print("EXCEPTION: " + str(e))
 
         return PostNoticeResponse(
@@ -56,7 +57,36 @@ class NotificationServiceServicer(NotificationServiceServicer, metaclass=Service
 
     @verified
     def CountNoReadPush(self, request, context):
-        pass
+        count = 0
+
+        result_code = ResultCode.UNKNOWN_RESULT_CODE
+        result_message = "Unknown count no read push Result"
+
+        db = pwdb.database
+
+        with db.atomic() as transaction:
+            try:
+                query = (PushLog.select(fn.count(PushLog.id).alias('count'))
+                         .where((PushLog.receiver_email == context.login_email) & PushLog.is_read))
+
+                for row in query:
+                    count = row.count
+
+                result_code = ResultCode.SUCCESS
+                result_message = "count no read success"
+
+            except Exception as e:
+                transaction.rollback()
+                result_code = ResultCode.ERROR
+                result_message = str(e)
+                print("EXCEPTION: " + str(e))
+
+        return PostNoticeResponse(
+            result=CommonResult(
+                result_code=result_code,
+                message=result_message,
+            ),
+        )
 
     @verified
     def GetPushList(self, request, context):
