cc73317c15ab816dd0e20a8842d14d6bf0400d52
hsh <tjdgus3537@naver.com>
2019-10-26 19:14:37 +0900

    PostNotice

---
+++ app/services/NotificationService.py
@@ -0,0 +1,68 @@
+from sea.servicer import ServicerMeta
+
+from app.decorators import verified
+from app.extensions import pwdb
+from app.models import NoticeModel, User, PushLog
+from protos.CommonResult_pb2 import ResultCode, CommonResult
+from protos.NotificationService_pb2 import PostNoticeResponse
+from protos.NotificationService_pb2_grpc import NotificationServiceServicer
+
+
+class NotificationServiceServicer(NotificationServiceServicer, metaclass=ServicerMeta):
+    @verified
+    def PostNotice(self, request, context):
+        notice = request.notice
+
+        title = notice.title
+        content = notice.content
+
+        result_code = ResultCode.UNKNOWN_RESULT_CODE
+        result_message = "Unknown post notice Result"
+
+        db = pwdb.database
+
+        with db.atomic() as transaction:
+            try:
+                # notice를 작성
+                NoticeModel.create(
+                    title=title,
+                    content=content,
+                )
+
+                user_query = User.select()
+
+                # 모든 사용자에게 push를 보내는 부분(지금은 db 저장만 수행)
+                for row in user_query:
+                    PushLog.create(
+                        receiver_email=row.email,
+                        content=content,
+                    )
+
+                result_code = ResultCode.SUCCESS
+                result_message = "post notice success"
+
+            except Exception as e:
+                transaction.rollback()
+                result_code = ResultCode.ERROR
+                result_message = str(e) + str(type(context.login_email))
+                print("EXCEPTION: " + str(e))
+
+        return PostNoticeResponse(
+            result=CommonResult(
+                result_code=result_code,
+                message=result_message,
+            ),
+        )
+
+    @verified
+    def CountNoReadPush(self, request, context):
+        pass
+
+    @verified
+    def GetPushList(self, request, context):
+        pass
+
+    @verified
+    def ReadPush(self, request, context):
+        pass
+
