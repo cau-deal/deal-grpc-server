9c0c25c2bf6ddb32d2123b7e4b85d9215ba5f628
hsh <tjdgus3537@naver.com>
2019-10-27 03:56:39 +0900

    Test print join result

--- app/services/missionService.py
+++ app/services/missionService.py
@@ -244,7 +244,7 @@ class MissionServiceServicer(MissionServiceServicer, metaclass=ServicerMeta):
 
                     if mission_type != MissionType.ALL_MISSION_TYPE:
                         query = (MissionModel
-                                 .select().join(MEI, JOIN.LEFT_OUTER, on=(MissionModel.id == MEI.mission_id))
+                                 .select(MissionModel, MEI).join(MEI, JOIN.LEFT_OUTER, on=(MissionModel.id == MEI.mission_id))
                                  .where((MissionModel.id >= _offset) & (MissionModel.id >= _offset) &
                                 (MEI.image_type == MissionExplanationImageType.THUMBNAIL_MISSION_EXPLANATION_IMAGE_TYPE)
                                        & (MissionModel.mission_type == MISSION_TYPE[mission_type]))
@@ -304,8 +304,7 @@ class MissionServiceServicer(MissionServiceServicer, metaclass=ServicerMeta):
                 search_mission_result = SearchMissionResult.SUCCESS_SEARCH_MISSION_RESULT
 
                 for row in query:
-                    for x in row:
-                        result_message += "   " + str(x)
+                    result_message += "  " + str(row.MEI.url)
 
             except Exception as e:
                 transaction.rollback()
