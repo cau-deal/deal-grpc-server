e099255370f1f6d8290fcb648ee2f95cf67a1d10
hsh <tjdgus3537@naver.com>
2019-10-26 19:47:56 +0900

    GetPushList

--- app/services/NotificationService.py
+++ app/services/NotificationService.py
@@ -4,9 +4,11 @@ from sea.servicer import ServicerMeta
 from app.decorators import verified
 from app.extensions import pwdb
 from app.models import NoticeModel, User, PushLog
-from protos.CommonResult_pb2 import ResultCode, CommonResult
-from protos.NotificationService_pb2 import PostNoticeResponse, ReadPushResponse, CountNoReadPushResponse
-from protos.NotificationService_pb2_grpc import NotificationServiceServicer
+
+from protos.CommonResult_pb2 import *
+from protos.Datetime_pb2 import *
+from protos.NotificationService_pb2 import *
+from protos.NotificationService_pb2_grpc import *
 
 
 class NotificationServiceServicer(NotificationServiceServicer, metaclass=ServicerMeta):
@@ -67,7 +69,7 @@ class NotificationServiceServicer(NotificationServiceServicer, metaclass=Service
         with db.atomic() as transaction:
             try:
                 query = (PushLog.select(fn.count(PushLog.id).alias('count'))
-                         .where((PushLog.receiver_email == context.login_email) & PushLog.is_read))
+                         .where((PushLog.receiver_email == context.login_email) & (not PushLog.is_read)))
 
                 for row in query:
                     count = row.count
@@ -91,7 +93,63 @@ class NotificationServiceServicer(NotificationServiceServicer, metaclass=Service
 
     @verified
     def GetPushList(self, request, context):
-        pass
+        push_lists = []
+
+        is_read_push_type = request.is_read_push_type
+
+        IS_READ_PUSH_TYPE = {
+            IsReadPushType.UNKNOWN_IS_READ_PUSH_TYPE: 0,
+            IsReadPushType.NOT_READ_IS_READ_PUSH_TYPE: 1,
+            IsReadPushType.READ_IS_READ_PUSH_TYPE: 2,
+            IsReadPushType.ALL_IS_READ_PUSH_TYPE: 3,
+        }
+
+        result_code = ResultCode.UNKNOWN_RESULT_CODE
+        result_message = "Unknown get push list Result"
+
+        db = pwdb.database
+
+        with db.atomic() as transaction:
+            try:
+                if is_read_push_type == IS_READ_PUSH_TYPE[IsReadPushType.NOT_READ_IS_READ_PUSH_TYPE]:
+                    query = (PushLog.select().where((PushLog.receiver_email == context.login_email) &
+                                                    (not PushLog.is_read)))
+                elif is_read_push_type == IS_READ_PUSH_TYPE[IsReadPushType.READ_IS_READ_PUSH_TYPE]:
+                    query = (PushLog.select().where((PushLog.receiver_email == context.login_email) &
+                                                    (PushLog.is_read)))
+                elif is_read_push_type == IS_READ_PUSH_TYPE[IsReadPushType.ALL_IS_READ_PUSH_TYPE]:
+                    query = (PushLog.select().where((PushLog.receiver_email == context.login_email)))
+                else:
+                    raise Exception("UNKNOWN_IS_READ_PUSH_TYPE")
+
+                result_code = ResultCode.SUCCESS
+                result_message = "get push list success"
+
+                for row in query:
+                    c = row.created_at
+                    push_lists.append(
+                        Push(
+                            id=row.id,
+                            content=row.content,
+                            is_read=row.is_read,
+                            created_at=Datetime(year=c.year, month=c.month, day=c.day,
+                                                hour=c.hour, min=c.minute, sec=c.second)
+                        )
+                    )
+
+            except Exception as e:
+                transaction.rollback()
+                result_code = ResultCode.ERROR
+                result_message = str(e)
+                print("EXCEPTION: " + str(e))
+
+        return GetPushListResponse(
+            result=CommonResult(
+                result_code=result_code,
+                message=result_message,
+            ),
+            push_list=push_lists,
+        )
 
     @verified
     def ReadPush(self, request, context):
