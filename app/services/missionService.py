c6009516c6983c9b6661975921dde27e29accd31
hsh <tjdgus3537@naver.com>
2019-10-28 04:14:19 +0900

    print val

--- app/services/missionService.py
+++ app/services/missionService.py
@@ -675,7 +675,8 @@ class MissionServiceServicer(MissionServiceServicer, metaclass=ServicerMeta):
                 user_name_query = (PhoneAuthentication.select()
                                    .where(PhoneAuthentication.user_email == register_email))
 
-                s += str(user_name_query) + "  " + str(type(user_name_query))
+                for row in user_name_query:
+                    s += str(row) + "  " + str(type(row))
                 """
                 #for row in user_name_query:
                 #    s += "  " + str(row) + "  " + str(type(row))
