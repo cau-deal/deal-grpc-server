b1edec7f318f8573ea0b2ac57df0afe056a7f31f
hsh <tjdgus3537@naver.com>
2019-10-27 15:38:05 +0900

    Fix bug

--- app/services/missionService.py
+++ app/services/missionService.py
@@ -40,7 +40,6 @@ class MissionServiceServicer(MissionServiceServicer, metaclass=ServicerMeta):
         order_package_quantity = mission.order_package_quantity
         deadline = mission.deadline
         beginning = mission.beginning
-        s = ('' + str(beginning) + "  " + str(type(beginning)))
         deadline_datetime = datetime.datetime(year=deadline.year, month=deadline.month, day=deadline.day,
                                      hour=deadline.hour, minute=deadline.min, second=deadline.sec)
         beginning_datetime = datetime.datetime(year=beginning.year, month=beginning.month, day=beginning.day,
