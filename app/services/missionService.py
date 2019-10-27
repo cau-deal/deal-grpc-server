9838fba6d694e5e766bd028ae1edf8ec18723771
hsh <tjdgus3537@naver.com>
2019-10-28 03:39:55 +0900

    Print all key-val

--- app/services/missionService.py
+++ app/services/missionService.py
@@ -677,10 +677,11 @@ class MissionServiceServicer(MissionServiceServicer, metaclass=ServicerMeta):
 
                     user_query = (User.select().where(User.email == register_email))
                     user_name_query = (PhoneAuthentication.select().where(PhoneAuthentication.user_email == register_email))
-                    """
+
                     for row in user_query.dicts():
                         s += str(row) + "  " + register_email
 
+                    """
                     for row in user_name_query.dicts:
                         user_name = row.name
 
