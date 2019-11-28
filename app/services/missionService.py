06fc117fbf6bb0ddbca774b08ea2e6a97c67c8d3
hsh <tjdgus3537@naver.com>
2019-11-29 02:43:05 +0900

    Add error print

--- app/services/missionService.py
+++ app/services/missionService.py
@@ -1085,7 +1085,7 @@ class MissionServiceServicer(MissionServiceServicer, metaclass=ServicerMeta):
                 query = (RecommendMission.select())
 
                 for row in query:
-                    s += row.url
+                    s += row.url + "   " + str(row.mission_id)
                     mission_recommend_images.append(
                         MissionRecommendImage(
                             mission_id=row.mission_id,
